150b7e85efa673e0af46ef0da8cb996db339c13c
Pascal <pacarole@gmail.com>
2017-06-20 09:00:05 -0400

    Progress on setting up strategy

--- functions/trade_strategy_1/index.js
+++ functions/trade_strategy_1/index.js
@@ -4,11 +4,9 @@ const stats = require('simple-statistics');
 const datastore = require('@google-cloud/datastore')({ promise: Promise });
 const pubsub = require('@google-cloud/pubsub')({ promise: Promise });
 
-const currencyChangeBroadcastTopic = pubsub.topic('new-currency-data');
+const newTransactionBroadcastTopic = pubsub.topic('new-transaction');
 
-const CURRENCY_AGGREGATION_MINUTES = 15;
 const MS_PER_MINUTE = 60000;
-const currencies = ['BTC', 'ETH', 'ETC', 'XRP', 'NXT', 'LTC', 'ZEC', 'DASH', 'STR', 'XMR', 'REP'];
 
 /**
  * Triggered from a message on a Cloud Pub/Sub topic.
@@ -16,77 +14,40 @@ const currencies = ['BTC', 'ETH', 'ETC', 'XRP', 'NXT', 'LTC', 'ZEC', 'DASH', 'ST
  * @param {!Object} event The Cloud Functions event.
  * @param {!Function} The callback function.
  */
-exports.update = (event, callback) => {
-  datastore.get(poloniexApiDataStoreKey).then((entity) => {
-    const poloniexApiKey = entity['API_KEY'];
-    const poloniexApiSecret = entity['SECRET'];
-    const poloniexClient = new Poloniex(poloniexApiKey, poloniexApiSecret);
-    const dateTime = new Date();
-
-    poloniexClient.returnTicker((err, tickerData) => {
-      if (!err) {
-        let currencyData = [];
-        _.forOwn(tickerData, (data, currencyPair) => {
-          currencyData.push({
-            key: datastore.key(['ticker']),
-            data: formatCurrencyData(currencyPair, data, dateTime)
-          });
-        });
-
-        datastore.save(currencyData)
-          .then(broadcastTickerChange)
-          .then(getOldTickerEntries)
-          .then(deleteTickerEntries)
-          .then(() => {
-            callback();
-          }).catch(callback);
-      } else {
-        callback(err);
-      }
-    });
+exports.buyOrSell = (event, callback) => {
+  getAccountInfo().then((accountInfo) => {
+    chooseToBuyOrSell(accountInfo).then(() => {
+      callback();
+    }).catch(callback);
   }).catch(callback);
 };
 
-const formatCurrencyData = (currencyPair, data, dateTime) => {
-  return {
-    currencyPair: currencyPair,
-    dateTime: dateTime,
-    last: parseFloat(data.last),
-    lowestAsk: parseFloat(data.lowestAsk),
-    highestBid: parseFloat(data.highestBid),
-    percentChange: parseFloat(data.percentChange),
-    baseVolume: parseFloat(data.baseVolume),
-    quoteVolume: parseFloat(data.quoteVolume),
-    isFrozen: data.isFrozen === '1',
-    high24hr: parseFloat(data.high24hr)
-  };
-
-
-  /*
-    let samples = [[0, 0], [1, 1]];
-    let regression = stats.linearRegression(samples);
-    let slope = regression.m;
-    let regressionLine = stats.linearRegressionLine(regression);
-    let volatility = stats.rSquared(samples, regressionLine); // = 1 this line is a perfect fit
-  */
+const chooseToBuyOrSell = (accountInfo) => {
+  if(accountInfo.lastAction == 'SELL') {
+    return buy(accountInfo);
+  } else {
+    return sell(accountInfo);
+  }
 }
 
-const broadcastCurrencyDataChange = () => {
+const getAccountInfo = () => {
+  // if no current transcations
+  
+}
+
+const buy = (accountInfo) => {
+  
+}
+
+const sell = (accountInfo) => {
+
+}
+
+const broadcastNewTransactionChange = (currency, price, quantity) => {
   return currencyChangeBroadcastTopic.publish('currency data updated');
 }
 
-const getCurrencyEntries = (currency) => {
-  const query = datastore.createQuery('ticker');
-  const currentDate = new Date();
-  const maxAgeDate = new Date(currentDate.getTime() - CURRENCY_AGGREGATION_MINUTES * MS_PER_MINUTE);
-  query.filter('dateTime', '<', maxAgeDate);
-  query.filter('currencyPair', 'USDT_'+currency);
-  query.order('dataTime', { descending: true });
-
+const getCurrencyData = () => {
+  const query = datastore.createQuery('currency-data');
   return datastore.runQuery(query);
-}
-
-const deleteTickerEntries = (entities) => {
-  const keys = _.map(entities[0], datastore.KEY);
-  return datastore.delete(keys);
 }
