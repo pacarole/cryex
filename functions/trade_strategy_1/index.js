ae73c25ef7a204f81a684befee3ad7e5ea37acde
Pascal <pacarole@gmail.com>
2017-06-23 11:10:41 -0400

    Finish buy algorithm

--- functions/trade_strategy_1/index.js
+++ functions/trade_strategy_1/index.js
@@ -10,6 +10,8 @@ const poloniexApiDataStoreKey = datastore.key(['poloniex_api', 'strategy1']);
 const MS_PER_MINUTE = 60000;
 const MAX_BUY_DIVIDER = 1/3;
 
+let poloniexClientSingleton, poloniexReturnBalances, poloniexBuy, poloniexSell;
+
 /**
  * Triggered from a message on a Cloud Pub/Sub topic.
  *
@@ -19,18 +21,23 @@ const MAX_BUY_DIVIDER = 1/3;
 exports.buyOrSell = (event, callback) => {
   const batchRequests = [getPoloniexClient(), getCurrencyData(), getAccountInfo()];
   
- Promise.all(backRequests).then(([poloniexClient, currencyDataEntities, accountInfo]) => {
-    chooseToBuyOrSell(poloniexClient, currencyDataEntities[0], accountInfo).then(() => {
+  Promise.all(backRequests).then(([poloniexClient, currencyDataEntities, accountInfo]) => {
+    poloniexClientSingleton = poloniexClient;
+    poloniexReturnBalances = Promise.promisify(poloniexClientSingleton.returnBalances);
+    poloniexBuy = Promise.promisify(poloniexClientSingleton.buy);
+    poloniexSel = Promise.promisify(poloniexClientSingleton.sell);
+   
+    chooseToBuyOrSell(currencyDataEntities[0], accountInfo).then(() => {
       callback();
     }).catch(callback);
   }).catch(callback);
 };
 
-const chooseToBuyOrSell = (poloniexClient, currencyData, accountInfo) => {
+const chooseToBuyOrSell = (currencyData, accountInfo) => {
   if(accountInfo.lastAction == 'SELL') {
-    return buy(poloniexClient, currencyData, accountInfo);
+    return buy(currencyData, accountInfo);
   } else {
-    return sell(poloniexClient, currencyData, accountInfo);
+    return sell(currencyData, accountInfo);
   }
 }
 
@@ -51,8 +58,7 @@ const getAccountInfo = () => {
   
 }
 
-const buy = (currencyData, accountInfo) => {
- 
+const buy = (currencyData) => {
   // sort currency data w/ positive slope by slope * volatilityFactor
   let filteredCurrencyData = _.filter(currencyData, (datum) => {
     return datum.slope > 0;
@@ -61,17 +67,38 @@ const buy = (currencyData, accountInfo) => {
     return datum.slope * datum.volatilityFactor;
   });
   filteredCurrencyData.reverse();
- 
   
-  
-  
-  // make buy decisions
+  return new Promise((resolve, reject) => {
+    poloniexReturnBalances.then((balances) => {
+      let maxBuyCash;
+      
+      Promise.each(filteredCurrencyData, (currencyInfo) => {
+        return poloniexReturnBalances.then((balances) => {
+          let availableCash = parseFloat(balances.USDT);
+          if(_.isUndefined(maxBuyAmount)) maxBuyCash = availableCash * MAX_BUY_DIVIDER;
+          
+          return makeBuyDecision(maxBuyCash, availableCash, currencyInfo);
+        });
+      }).then(resolve).catch(reject);
+    }).catch(reject);                  
+  });
 }
 
-const makeBuyDecision = (availableCash, maxBuyAmount, currencyInfo) => {
-  // (currentPrice - pastPrice) / pastPrice > 5 - 4 * volatilityFactor
-  // maxBuyAmount
+const makeBuyDecision = (maxBuyCash, availableCash, currencyInfo) => {
+  const buyCash = availableCash < maxBuyCash ? availableCash : maxBuyCash;
+  const priceIncreasePercentage = (currencyInfo.currentPrice - currencyInfo.pastPrice) / currencyInfo.pastPrice * 100;
+  const stabilityThreshold = 5 - 4 * currencyInfo.volatilityFactor;
+  const shouldBuy = priceIncreasePercentage > stabilityThreshold;
   
+  if(buyCash > 0 && shouldBuy) {
+    const currencyPair = 'USDT_' + currencyInfo.name;
+    const rate = currencyInfo.currentPrice;
+    const amount = buyCash / rate;
+    
+    return poloniexBuy(currencyPair, rate, amount, false /* fillOrKill */, true /* immediateOrCancel */); 
+  } else {
+    Promise.resolve();
+  }
 }
 
 const sell = (currencyData, accountInfo) => {
