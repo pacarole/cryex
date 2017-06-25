cd98e46c8075d1f5fcb31119e310ac189b7b51f7
Pascal <pacarole@gmail.com>
2017-06-24 21:59:20 -0400

    compute short slope angle

--- functions/currency_data/index.js
+++ functions/currency_data/index.js
@@ -7,6 +7,7 @@ const pubsub = require('@google-cloud/pubsub')({ promise: Promise });
 const currencyChangeBroadcastTopic = pubsub.topic('new-currency-data');
 
 const CURRENCY_AGGREGATION_MINUTES = 15;
+const SHORT_CURRENCY_AGGREGATION_MINUTES = 5;
 const MS_PER_MINUTE = 60000;
 
 /**
@@ -27,6 +28,7 @@ exports.update = (event, callback) => {
 const aggregateAndSaveCurrencyData = (entities) => {
   const currentDate = new Date();
   const maxAgeDate = new Date(currentDate.getTime() - CURRENCY_AGGREGATION_MINUTES * MS_PER_MINUTE);
+  const shortMaxAgeDate = new Date(currentDate.getTime() - SHORT_CURRENCY_AGGREGATION_MINUTES * MS_PER_MINUTE);
 
   const filteredEntities = _.filter(entities[0], (entity) => {
     return entity.dateTime > maxAgeDate;
@@ -47,16 +49,23 @@ const aggregateAndSaveCurrencyData = (entities) => {
 
   let currencyData = [];
   _.forOwn(dataForCurrencies, (data, currency) => {
+    let primaryCurrencyData = aggregateCurrencyData(currency, data, CURRENCY_AGGREGATION_MINUTES);
+    const shortData = _.filter(data, (row) => {
+      return row.dateTime > shortMaxAgeDate;
+    });
+    const shortCurrencyData = aggregateCurrencyData(currency, shortData, SHORT_CURRENCY_AGGREGATION_MINUTES);
+    primaryCurrencyData.shortSlopeAngle = shortCurrencyData.slopeAngle;
+    
     currencyData.push({
       key: datastore.key(['currency_data', currency]),
-      data: aggregateCurrencyData(currency, data)
+      data: primaryCurrencyData
     });
   });
 
   return datastore.save(currencyData);
 }
 
-const aggregateCurrencyData = (currency, data) => {
+const aggregateCurrencyData = (currency, data, slopeSpan) => {
   // guarantee order by date
   const tickerRows = _.sortBy(data, (row) => {
     return row.dateTime.getTime();
@@ -76,13 +85,13 @@ const aggregateCurrencyData = (currency, data) => {
 
   const regression = stats.linearRegression(samples);
   const regressionLine = stats.linearRegressionLine(regression);
-  const lastPoint = regressionLine(CURRENCY_AGGREGATION_MINUTES) - regression.b;
+  const lastPoint = regressionLine(slopeSpan) - regression.b;
   
   return {
     currentPrice: newestRow.last,
     pastPrice: oldestRow.last,
     slope: regression.m,
-    slopeAngle: Math.atan(lastPoint / CURRENCY_AGGREGATION_MINUTES) * 180 / Math.PI, 
+    slopeAngle: Math.atan(lastPoint / slopeSpan) * 180 / Math.PI, 
     volatilityFactor: stats.rSquared(samples, regressionLine),
     volume24h: newestRow.baseVolume
   };
